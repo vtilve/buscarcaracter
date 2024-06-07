0b2332ececc55aa31795e20862ff51f956398022
vtilve <victortilve@cifpcarballeira.com>
2024-06-07 19:33:49 +0200

    tercer cambio

--- Main.java
+++ Main.java
@@ -10,8 +10,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {        
-        char C = 'R';
-        char[] V = {'A','A','A','A','A','A','A','K','A','A','A'};
+        char C = 'K';
+        char[] V = {'A','A','A','A','A','A','A','A','A','K','A'};
         OperacionsArrays instance = new OperacionsArrays();
         boolean result = instance.busca(C, V);
         System.out.print(result + "\t");
