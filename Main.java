5825b171c23d933e56f0a784f0419e85ae44f597
vtilve <victortilve@cifpcarballeira.com>
2024-06-07 18:54:49 +0200

    inicio proyecto

---
+++ Main.java
@@ -0,0 +1,19 @@
+package buscarcaracter;
+
+/**
+ *
+ * @author Profesor
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {        
+        char C = 'K';
+        char[] V = {'A','A','A','A','K','A','A','A','A','A','A'};
+        OperacionsArrays instance = new OperacionsArrays();
+        boolean result = instance.busca(C, V);
+        System.out.print(result + "\t");
+    }
+}
